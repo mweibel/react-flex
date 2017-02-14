eb069c28264d8bfa832f1f55bb4bdcb50b73b175
zippy-react-dev <radu.brehar@zippyui.com>
2017-02-14 13:42:27 +0200

    Add license header to source files

--- src/Item.js
+++ src/Item.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2015-present Zippy Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { PropTypes } from 'react';
 import assign from 'object-assign';
 import Component from '@zippytech/react-class';
@@ -9,7 +25,7 @@ import cleanup from './cleanup';
 class ZippyFlexItem extends Component {
   render() {
     const props = this.props;
-    const className = join('react-flex-item', props2className(props));
+    const className = join('zippy-react-flex-item', props2className(props));
 
     const allProps = assign({}, props);
 
